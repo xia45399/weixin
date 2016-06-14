a27feb80099b0ade37594adfba8b4e37769299f4
xialiming <453993887@qq.com>
2016-06-14 14:15:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xia/weixin/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.xia.weixin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.context=this;
+        findViewById(R.id.service_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openService();
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Weixin.kind=2;
+                startMM();
+            }
+        });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Weixin.kind=3;
+                startMM();
+            }
+        });
+        findViewById(R.id.button0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMM();
+            }
+        });
+    }
+
+
+
+    private void openService() {
+        try {
+            Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(intent);
+            Toast.makeText(this, "找到Weixin，开启服务即可", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void startMM()
+    {
+        PackageManager packageManager=context.getPackageManager();
+        Intent intent = new Intent();
+        intent = packageManager.getLaunchIntentForPackage("com.tencent.mm");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Intent.FLAG_ACTIVITY_CLEAR_TOP) ;
+        context.startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/xia/weixin/Weixin.java
@@ -0,0 +1,107 @@
+package com.example.xia.weixin;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+
+/**
+ * Created by xia on 2016/6/13.
+ */
+public class Weixin extends AccessibilityService{
+
+    public static int kind=0;
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        int eventType=event.getEventType();
+        switch (eventType) {
+            case 64:
+                List<CharSequence> t64=event.getText();
+                break;
+
+            case 32:
+                switch (kind)
+                {
+                    case 1:
+                        //抢红包
+                    case 2://附近好友
+                        handle2();
+                        break;
+                    case 3:
+                        handle3();//通讯录添加好友
+                        break;
+                }
+
+                break;
+        }
+    }
+
+    private void handleUpdate()
+    {
+
+    }
+    private void handle2()
+    {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+
+        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("发现");
+        if(list.size()>0)
+        {
+            AccessibilityNodeInfo parent = list.get(0).getParent();
+            if(parent != null)
+            {
+                parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);//点击发现
+            }
+        }
+
+    }
+    private void handle3()
+    {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+
+        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("通讯录");
+        if(list.size()>0)
+        {
+            AccessibilityNodeInfo parent = list.get(0).getParent();
+            if(parent != null)
+            {
+                parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);//点击通讯录
+            }
+        }
+
+        List<AccessibilityNodeInfo> list1=nodeInfo.findAccessibilityNodeInfosByText("新的朋友");
+        if(list.size()>0)
+        {
+            AccessibilityNodeInfo node=list1.get(0);
+            AccessibilityNodeInfo parent=node.getParent().getParent();
+            if(parent!=null) {
+                parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+
+        //判断是否有接收字样
+        AccessibilityNodeInfo nodeInfo2 = getRootInActiveWindow();
+        List<AccessibilityNodeInfo> list2 = nodeInfo2.findAccessibilityNodeInfosByText("接受");
+
+        for(AccessibilityNodeInfo node:list2)//有新的朋友添加，点通过
+        {
+            node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            //通过了点击返回
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
+
+        }
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
